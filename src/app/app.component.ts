7b43865f217111417a24d8270c6f2a5991224681
Tran Son Hoang <hayden.tran.d@gmail.com>
2019-06-13 15:08:09 +0800

    config jest and remove karma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,11 @@
 
 import { Component, OnInit } from '@angular/core';
-import { NricService } from 'nric';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'NRIC';
-
-  constructor(private icService: NricService) {}
-
-  ngOnInit(): void {
-    console.log('testnric service', this.icService.isNRICValid('560224-10-8354'));
-
-  }
 }
