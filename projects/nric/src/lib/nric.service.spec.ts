e076c195a41fbc3551fc0a2479bc4fb099ac1ef2
Tran Son Hoang <hayden.tran.d@gmail.com>
2019-06-13 16:58:22 +0800

    add testing

--- projects/nric/src/lib/nric.service.spec.ts
+++ projects/nric/src/lib/nric.service.spec.ts
@@ -1,7 +1,29 @@
-const { NricService } = require('./nric.service');
+import { NricService } from './nric.service'
 
-describe('Movies API', () => {
-    it('should create a new movie', () => {
-        expect(true).toEqual(true)
+describe('NricService', () => {
+
+    const PASSES = ['560224108354','560224-10-8354', '010224108354'];
+    const FAILS = ['1ad224-10-8354', '123234', `asd@$as12dasd`];
+    const service = new NricService();
+
+    PASSES.forEach( value => {
+        it(`${value} should be valid`, () => {
+            const actual = service.isNRICValid(value);
+            expect(actual).toEqual(true)
+        })
+    })
+
+    FAILS.forEach( value => {
+        it(`${value} should be invalid`, () => {
+            expect( () => {
+                service.isNRICValid(value);
+            }).toThrowError('Invalid value number format');
+        })
+    })
+
+    it('random some IC number', () => {
+        const newIC = service.randomNRICNumber();
+        console.log(newIC);
+        expect(newIC.length).toEqual(12);
     })
 })
--- projects/nric/src/lib/nric.service.ts
+++ projects/nric/src/lib/nric.service.ts
@@ -3,6 +3,7 @@ import { I_NRIC } from './service.interface';
 
 import { splitNRIC, combineToDate } from './service.helpers';
 import { isStateValid } from './state.helpers';
+import { generateRandom } from './random.helpers';
 
 @Injectable()
 export class NricService implements I_NRIC {
@@ -12,12 +13,11 @@ export class NricService implements I_NRIC {
   isNRICValid(value: string): boolean {
     const [ input, year , month , day ,birthPlace, ...rest] = splitNRIC(value);
     const birthDate = combineToDate(year, month, day);
-
-    return !birthDate && isStateValid(birthPlace);
+    return birthDate && isStateValid(birthPlace);
   }
 
   randomNRICNumber(): string {
-    throw new Error("Method not implemented.");
+    return generateRandom();
   }
 
   formatNRICNumber(value: string, delimiter?: string): string {
